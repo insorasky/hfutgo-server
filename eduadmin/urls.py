61e3cbff4782a5df16679fc4b552299873d50bfc
Sora <petzuo@yahoo.co.jp>
2021-05-14 01:08:17 +0800

    更正路由

--- eduadmin/urls.py
+++ eduadmin/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # path('all_schedule/', include('eduadmin.apis.all_schedule.urls'), name='eduadmin_all_schedule'),
     path('classroom/', include('eduadmin.apis.classroom.urls'), name='eduadmin_classroom'),
     path('schedule/', include('eduadmin.apis.schedule.urls'), name='eduadmin_schedule'),
-    path('login/', Login.as_view(), name='eduadmin_login'),
+    path('login', Login.as_view(), name='eduadmin_login'),
     path('exams', Exams.as_view(), name='eduadmin_exams'),
 ]
