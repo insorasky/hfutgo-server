eaf393663e8f484cacb3c7684d92dcfa56248a1f
Sora <petzuo@yahoo.co.jp>
2021-06-10 00:17:34 +0800

    主页余额修复

--- user/apis/today/Balance.py
+++ user/apis/today/Balance.py
@@ -4,7 +4,7 @@ from django.views import View
 
 class Balance(View):
     def get(self, request, stu, user):
-        balance = stu.request('/https/77726476706e69737468656265737421fff944d22f367d44300d8db9d6562d/api/operation/thirdPartyApi/schoolcard/balance?sno=' + info.id).json()['data']
+        balance = stu.request('/https/77726476706e69737468656265737421fff944d22f367d44300d8db9d6562d/api/operation/thirdPartyApi/schoolcard/balance?sno=' + user.student_id).json()['data']
         return get_json_response({
             'balance': balance,
         })
