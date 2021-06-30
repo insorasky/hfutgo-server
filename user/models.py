9c15e28549d7f9aa35db7639c9fe4b3d4356e167
Sora <petzuo@yahoo.co.jp>
2021-07-01 01:02:19 +0800

    游客模式开坑

--- user/models.py
+++ user/models.py
@@ -13,11 +13,21 @@ class User(models.Model):
 
 
 class LoginState(models.Model):
+    type = models.IntegerField(max_length=1, db_index=True)  # 正常学生：1，游客：2，封网临时用户：3
     user_id = models.IntegerField()
     student_id = models.IntegerField()
+    openid = models.CharField(max_length=64)
     token = models.UUIDField(db_index=True)
     vpn_ticket = models.TextField()
     at_token = models.TextField()
     available = models.BooleanField(default=True)
     create_time = models.DateTimeField(auto_now_add=True)
     destroy_time = models.DateTimeField(auto_now=True)
+
+
+class Guest(models.Model):
+    openid = models.CharField(max_length=64)
+    unionid = models.CharField(max_length=64)
+    nick_name = models.TextField()
+    city = models.TextField()
+    avatar = models.TextField()
