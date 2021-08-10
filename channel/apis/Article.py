1ce8b8dbffded8a5655024b8fd39e54e3980344c
Sora <petzuo@yahoo.co.jp>
2021-08-10 10:53:20 +0800

    通知内容完善

--- channel/apis/Article.py
+++ channel/apis/Article.py
@@ -1,7 +1,8 @@
-from bs4 import BeautifulSoup
 from django.views import View
 from utils.response import get_json_response
 import re
+from readability import Document
+from bs4 import BeautifulSoup
 
 url_pattern = r'http[s]?:\/\/(?:[a-zA-Z]+)\.hfut\.edu\.cn\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
@@ -18,11 +19,21 @@ class Article(View):
             )
             if '无法访问此网站' in data.text:
                 return get_json_response('无法访问此网站', 3552)
-            soup = BeautifulSoup(data.content, 'lxml').body
-            [s.extract() for s in soup.select('a, script, ul, li, button, input')]
+            doc = Document(data.content.decode())
+            html = doc.summary()
+            soup = BeautifulSoup(html, 'lxml')
+            if len(soup.text) < 20:
+                soup = BeautifulSoup(data.content, 'lxml').body
+                [s.extract() for s in soup.select('a, script, ul, li, button, input')]
+                return get_json_response({
+                    'html': ''.join([str(s) for s in soup]),
+                    'text': re.sub('\n+', '\n', soup.get_text()),
+                    'title': doc.title()
+                })
             return get_json_response({
-                'html': ''.join([str(s) for s in soup]),
-                'text': re.sub('\n+', '\n', soup.get_text())
+                'html': re.search(r'<html><body>(.*)</body></html>', html)[1],
+                'text': BeautifulSoup(html, 'lxml').text,
+                'title': doc.title()
             })
         else:
             return get_json_response('URL不合法！', 3551)
